b3c0c760222f7efe958904c96cbf116758e0cc5a
zhangjw <zhangjw@hit.cn>
2025-01-23 19:22:37 +0800

    图片处理开发

---
+++ WpfMain/Logic/TestLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMain.Entity;
+
+namespace WpfMain.Logic
+{
+    public static class TestLogic
+    {
+        public static bool Save(TestInfo tInfo)
+        {
+            return false;
+        }
+    }
+}
