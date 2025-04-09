d23edb55c2d7900b358c9607322f0b1078a61be8
zhangjw <zhangjw@hit.cn>
2025-04-09 13:57:55 +0800

    优化

--- WpfMain/Module/PetModule/UCImageItemView.xaml.cs
+++ WpfMain/Module/PetModule/UCImageItemView.xaml.cs
@@ -7,7 +7,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using DevExpress.Utils.About;
+
 using DrawTools;
 using DrawTools.Utils;
 using Entity.Entity;
