ec6251dbc49652c7429bc7ca43a84488e3d36aec
zhangjw <zhangjw@hit.cn>
2025-04-15 11:48:55 +0800

    代码优化

--- WpfMain/Module/PetModule/FrmPet.xaml.cs
+++ WpfMain/Module/PetModule/FrmPet.xaml.cs
@@ -102,14 +102,11 @@ namespace WpfMain.Module.PetModule
                 //tInfo.Result = new TestResult();
                 //var sp = videoPath.Split('\\');
                 //var fileName = sp[sp.Length - 1];
-                tInfo.Result.Vedios.Add(new MediaItem() { Source = videoPath, Name = Path.GetFileName(videoPath), Type = MediaSourceType.LocalPath });
+                //old.Vedios.Add(new MediaItem() { Source = videoPath, Name = fileName, Type = MediaSourceType.LocalPath });
                 //tInfo.Result = old;
 
 
-
                 tInfo.Result.Vedios.Add(new MediaItem() { Source = videoPath, Name = Path.GetFileName(videoPath), Type = MediaSourceType.LocalPath });
-
-
                 VideoModel.ExposureModel = new Exposure() { IsAuto = VideoModel.ExposureModel.IsAuto, IsEnable = true };
                 StartCamp.Content = "开始录像"; 
             }
